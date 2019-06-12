bf9b846140e0c8ef7aae7b6c74980e5333a2caa3
Carl Saptarshi <carlsaptarshi@rentalcars.com>
2019-06-12 14:01:37 +0100

    created person query resolver which links to the type.graphqls types

---
+++ src/main/kotlin/com/example/demo/graphql/query/PersonQueryResolver.kt
@@ -0,0 +1,12 @@
+package com.example.demo.graphql.query
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import com.example.demo.service.PersonService
+import org.springframework.stereotype.Component
+
+@Component
+class PersonQueryResolver(val personService: PersonService): GraphQLQueryResolver {
+    fun person(id: String) = personService.getPersonById(id)
+    fun personsByName(name: String) = personService.getPersonsByName(name)
+
+}
